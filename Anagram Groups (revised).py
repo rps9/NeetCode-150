d0e86df7d7db21ecd52414ef004b32f41e77318b
Ryan Smith <147011183+rps9@users.noreply.github.com>
2024-08-30 17:02:40 -0400

    Update Anagram Groups (revised).py
    using .values() to return

--- Anagram Groups (revised).py
+++ Anagram Groups (revised).py
@@ -4,7 +4,6 @@ class Solution:
         # Creates an array of the number of characters for each word and uses that to hash
         # Biggest piece is realizing you need to make the array a tuple when you are done so you can actually hash into the hashtable
         word_count = {}
-        grouped_words = []
         for word in strs:
             char_count = [0] * 26
             for char in word:
@@ -14,9 +13,7 @@ class Solution:
             except:
                 word_count[tuple(char_count)] = [word]
         
-        for group in word_count:
-            grouped_words.append(word_count[group])
 
-        return grouped_words
+        return word_count.values()
 
         
