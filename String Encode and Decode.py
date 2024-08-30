80cf0e51f5806c28875e2cfd585eaa2ad0c25827
Ryan Smith <147011183+rps9@users.noreply.github.com>
2024-08-30 19:57:28 -0400

    Update String Encode and Decode.py

--- String Encode and Decode.py
+++ String Encode and Decode.py
@@ -4,22 +4,22 @@ class Solution:
         # Have each string start with the how many character it is then a special (non-number) character that tells the function when to stop reading the length
         single_string = ''
         for string in strs:
-            single_string += f'{len(string)}*{string}'
+            single_string += f'{len(string)}Ryan{string}' # Use my name as delimiter for fun but only one character is needed
         return single_string
     def decode(self, s: str) -> List[str]:
         # Initialize
         word_array = []
         i = 0
-        
+
         # Use a while loop to better control where we are in the array
         while i < len(s):
             # Read the number string until it tells you to stop
             number_string = ''
-            while s[i] != '*':
+            while s[i] != 'R':
                 number_string += s[i]
                 i += 1
             string_length = int(number_string)
-            i += 1 # Add one to get past the *
+            i += 4 # Add 4 to get past the word 'Ryan'
             word_array.append(s[i:i+string_length]) # Append the next length number of characters
             i += string_length # Move i to the beginning of the next length
 
