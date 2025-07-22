48e5485a802444413fc6c80064a31ef4ba93549b
Ryan Smith <147011183+rps9@users.noreply.github.com>
2025-07-22 15:57:19 -0400

    Solving Minimum Stack

---
+++ Minimum Stack.py
@@ -0,0 +1,30 @@
+'''
+The basic idea behind this implementation is having a stack that keeps track of the minimum value at each change to the
+main stack. 
+'''
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if not self.min_stack:
+            min_val = val
+        else:
+            min_val = min(val, self.min_stack[-1])
+        
+        self.min_stack.append(min_val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
